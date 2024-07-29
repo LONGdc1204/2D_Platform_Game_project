73ef115efc1cd1ffadb81ac2181c5d057130c657
Long Nguyen <73941824+LONGdc1204@users.noreply.github.com>
2024-07-30 05:43:05 +0700

    Update EndPoint.cs

--- Scripts/Point/EndPoint.cs
+++ Scripts/Point/EndPoint.cs
@@ -7,9 +7,10 @@ public class EndPoint : MonoBehaviour
     private Animator anim => GetComponent<Animator>();
     private void OnTriggerEnter2D(Collider2D other) {
         Player player = other.GetComponent<Player>();
-        if (player != null)
-        AudioManager.instance.PlaySFX(2);
+        if (player != null) {
+            AudioManager.instance.PlaySFX(2);
             anim.SetTrigger("endActivate");
             GameManager.instance.LevelFinish();
+        }
     }
 }
