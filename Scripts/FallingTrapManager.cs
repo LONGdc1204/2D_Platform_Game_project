f926d10d34e8856ff97a72cbb8a42b17a669a9e9
Long Nguyen <73941824+LONGdc1204@users.noreply.github.com>
2024-07-30 05:42:29 +0700

    Update FallingTrapManager.cs

--- Scripts/FallingTrapManager.cs
+++ Scripts/FallingTrapManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 public class FallingTrapManager : MonoBehaviour
 {
     [SerializeField] GameObject fallingTrapPrefab;
-    
-    public void ResetFallingTrap() {
-        StartCoroutine(ResetFallingTrapRoutine());
+    private void Start() {
+        Instantiate(fallingTrapPrefab, new Vector3(transform.position.x + 0, transform.position.y, transform.position.z), Quaternion.identity);
+        Instantiate(fallingTrapPrefab, new Vector3(transform.position.x + 5, transform.position.y, transform.position.z), Quaternion.identity);
+        Instantiate(fallingTrapPrefab, new Vector3(transform.position.x + 10, transform.position.y, transform.position.z), Quaternion.identity);
     }
-    private IEnumerator ResetFallingTrapRoutine() {
-        yield return new WaitForSeconds(6f);
-        Instantiate(fallingTrapPrefab, transform.position, Quaternion.identity);
+    public void ResetFallingTrap(float addPositionX) {
+        StartCoroutine(ResetFallingTrapRoutine(addPositionX));
+    }
+    private IEnumerator ResetFallingTrapRoutine(float addPositionX) {
+        yield return new WaitForSeconds(5f);
+        Instantiate(fallingTrapPrefab, new Vector3(transform.position.x + addPositionX, transform.position.y, transform.position.z), Quaternion.identity);
     }
 
 }
